d9ae241f29e9fd6adf264cf419db40b2be5fe9ef
Alexander Barth <barth.alexander@gmail.com>
2023-05-16 16:11:48 +0200

    update

---
+++ SmoothedParticleHydrodynamics/compile.jl
@@ -0,0 +1,99 @@
+include("../wasm_target.jl")
+
+using SmoothedParticleHydrodynamics
+using SmoothedParticleHydrodynamics: Particle, W, update!
+using StableRNGs
+using Random
+import Random: rand, AbstractRNG
+# assume that we use 32-bit julia
+@assert Int == Int32
+
+# need to inline because of named tuples
+
+
+seed = 123456789
+m = Int64(2)^31
+
+mutable struct LinearCongruentialGenerators <: AbstractRNG
+    seed::Int32
+end
+
+struct LinearCongruentialGenerators2 <: AbstractRNG
+    seed::Int32
+end
+
+struct LinearCongruentialGenerators3 <: AbstractRNG
+    seed::Ref{Int32}
+end
+
+rng = LinearCongruentialGenerators(42)
+
+function rand(rng::LinearCongruentialGenerators,::Type{Int32})
+    m = Int64(1) << 31
+    a = 1103515245
+    c = 12345
+    rng.seed = Int32((a * rng.seed + c) % m)
+    return rng.seed
+end
+
+function rand(rng::LinearCongruentialGenerators,::Type{Float32})
+    r = Int64(typemax(Int32)) - typemin(Int32)
+    return (Int64(rand(rng,Int32)) - typemin(Int32))/Float32(r)
+end
+
+# rand(rng,Float32)
+
+function model_step(grav,f,Δx,Δt,ntime,
+                    mask,
+                    particles,
+                    )
+
+
+#    rng = StableRNG(123)
+#    rng = Random.GLOBAL_RNG;
+    rng = LinearCongruentialGenerators(42)
+#    rng = LinearCongruentialGenerators2(42)
+#    rng = LinearCongruentialGenerators3(42)
+#    rng = 42
+
+    config,particles,W_spiky,W_rho =
+        SmoothedParticleHydrodynamics.case_dam_break2!(#rng,
+            particles,
+            init_particles = ntime == 0,
+            rng = rng,
+        )
+
+    update!(config,W_spiky,W_rho,particles)
+    return 0
+end
+
+grav,f,Δx,Δt,ntime = 0,0,0,0,0
+mask = MallocMatrix{Int32}(undef,(20,20))
+particles = MallocVector{Particle{2,Float32}}(undef,(20,))
+
+model_step(grav,f,Δx,Δt,ntime,
+                    mask,
+                    particles,
+                    )
+
+obj = build_obj(model_step, Tuple{
+    Float32,
+    Float32,
+    Float32,
+    Float32,
+    Int32,
+    MallocMatrix{Int32},
+    MallocVector{Particle{2,Float32}},
+})
+
+write("model.o", obj)
+
+# heap base: 66560
+
+# size of the total memory
+mem = 65536*16*2
+
+# the linker needs memset
+run(`clang --target=wasm32 --no-standard-libraries -c -o memset.o ../memset.c`)
+
+run(`wasm-ld --initial-memory=$(mem) --no-entry --export-all -o model.wasm memset.o model.o`)
---
+++ SmoothedParticleHydrodynamics/model_loader.js
@@ -0,0 +1,66 @@
+import { MallocArray, pcolor, quiver, mouse_edit_mask } from "../julia_wasm_utils.js";
+
+export async function run(document) {
+    const response = await fetch('model.wasm');
+    const bytes = await response.arrayBuffer();
+    const { instance } = await WebAssembly.instantiate(bytes);
+
+    const { julia_model_step, memory, __heap_base } = instance.exports;
+
+    // base[0] offset of memory, increased by MallocArray
+    let base = [__heap_base];
+
+    const sz = [300,100];
+    const dx = 5000;
+    var ntime = 0;
+    const nparticles = 1219
+    let scale = 0.2;
+
+    let [mask_p, mask] = MallocArray(Int32Array,memory,base,sz);
+    let [particles_p, particles] = MallocArray(Float32Array,memory,base,[8,nparticles]);
+
+    // canvas for plotting
+    const canvas = document.getElementById("plot");
+    const erase_elem = document.getElementById("erase");
+    const pen_size_elem = document.getElementById("pen_size");
+    const [ctx,res] = mouse_edit_mask(canvas,erase_elem,pen_size_elem,mask,sz);
+
+    function step(timestamp) {
+        let grav = parseFloat(document.getElementById("grav").value);
+        let f = parseFloat(document.getElementById("f").value);
+        let DeltaT = parseFloat(document.getElementById("DeltaT").value);
+        let pmin = parseFloat(document.getElementById("pmin").value);
+        let pmax = parseFloat(document.getElementById("pmax").value);
+        let show_velocity = document.getElementById("show_velocity").checked;
+
+        if (!isNaN(grav) && !isNaN(f) && !isNaN(pmin) && !isNaN(pmax) && !isNaN(DeltaT)) {
+            //console.log("p ",pressure[140 + sz[0] * 40]);
+
+            const result = julia_model_step(
+                grav,f,dx,DeltaT,ntime,
+                mask_p,particles_p);
+
+            ntime += 1;
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            //pcolor(ctx,sz,res,pressure,mask,{pmin: pmin, pmax: pmax});
+            console.log(particles[8*3 ]);
+
+            for (let i = 0; i < nparticles; i++) {
+                ctx.beginPath();
+                let x = scale * particles[8 * i + 0];
+                let y = scale * particles[8 * i + 1];
+                let radius = 2;
+                ctx.arc(x, y, radius, 0, 2 * Math.PI);
+                ctx.stroke();
+            }
+
+            if (show_velocity) {
+                //quiver(ctx,sz,res,u,v,mask,{subsample: 5, scale: 500});
+
+            }
+        }
+        window.requestAnimationFrame(step);
+    }
+
+    window.requestAnimationFrame(step);
+}
