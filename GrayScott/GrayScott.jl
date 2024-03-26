e4f34f1ad91c3939f0072bc3a141c28f8d586efd
Alexander Barth <barth.alexander@gmail.com>
2024-03-26 18:54:06 +0100

    clean-up

--- GrayScott/GrayScott.jl
+++ GrayScott/GrayScott.jl
@@ -8,24 +8,29 @@ module GrayScott
     FYu1 = (u[i,j] - u[i,j-1]) * (mask[i,j] & mask[i,j-1]) * invΔy
     FYu2 = (u[i,j+1] - u[i,j]) * (mask[i,j+1] & mask[i,j]) * invΔy
 
-    return ((FXu2 - FXu1) * Du_over_Δxy[1] + (FYu2 - FYu1) * Du_over_Δxy[1])
+    return ((FXu2 - FXu1) * Du_over_Δxy[1] + (FYu2 - FYu1) * Du_over_Δxy[2])
 end
 
 
-@inline @inbounds function diffusion(mask::Nothing,(invΔx,invΔy),Du,u,(i,j))
+@inline @inbounds function diffusion(mask::Nothing,(invΔx,invΔy),Du_over_Δxy,u,(i,j))
+    FXu1 = (u[i,j] - u[i-1,j]) * invΔx
+    FXu2 = (u[i+1,j] - u[i,j]) * invΔx
+
+    FYu1 = (u[i,j] - u[i,j-1]) * invΔy
+    FYu2 = (u[i,j+1] - u[i,j]) * invΔy
+
+    return ((FXu2 - FXu1) * Du_over_Δxy[1] + (FYu2 - FYu1) * Du_over_Δxy[2])
 end
 
 # where mask = 1: valid grid cell with fluid
 # where mask = 0: no fluid
 
 function step!(config,mask,(u,v),(un,vn))
-    ((Δx,Δy),(invΔx,invΔy),Δt,(Du,Dv),f,k) = config
+    (Δxy,invΔxy,Δt,(Du,Dv),f,k) = config
     sz = size(u)
 
-    invΔxy = (invΔx,invΔy)
-    Du_over_Δxy = Du .* (invΔx,invΔy)
-    Dv_over_Δxy = Dv .* (invΔx,invΔy)
-
+    Du_over_Δxy = Du .* invΔxy
+    Dv_over_Δxy = Dv .* invΔxy
     @inbounds for j = 2:sz[2]-1
         for i = 2:sz[1]-1
             uv2 = u[i,j]*v[i,j]^2
