5271c9ce1d0e20e0fb8453bd69b24f9a7d85610f
Alexander Barth <barth.alexander@gmail.com>
2024-03-26 17:47:34 +0100

    add GrayScott.jl

---
+++ GrayScott/GrayScott.jl
@@ -0,0 +1,20 @@
+module GrayScott
+
+function step!(config,(u,v),(un,vn))
+    (Δx,Δt,(Du,Dv),f,k) = config
+
+    sz = size(u)
+    @inbounds for j = 2:sz[2]-1
+        for i = 2:sz[1]-1
+            uv2 = u[i,j]*v[i,j]^2
+            un[i,j] = u[i,j] + Δt * (Du * (u[i-1,j] + u[i+1,j] + u[i,j-1] + u[i,j+1] - 4*u[i,j]) / Δx^2 - uv2 + f * (1 - u[i,j]))
+
+            vn[i,j] = v[i,j] + Δt * (Dv * (v[i-1,j] + v[i+1,j] + v[i,j-1] + v[i,j+1] - 4*v[i,j]) / Δx^2 + uv2 - (f + k) * v[i,j])
+        end
+    end
+
+    @inbounds u .= un
+    @inbounds v .= vn
+end
+
+end
