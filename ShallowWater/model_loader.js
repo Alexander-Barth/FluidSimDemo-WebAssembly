c0c293f24f5998dab928c364da9d7c9620437a4f
Alexander Barth <barth.alexander@gmail.com>
2024-04-26 10:19:15 +0200

    variable Coriolis parameter

--- ShallowWater/model_loader.js
+++ ShallowWater/model_loader.js
@@ -15,13 +15,31 @@ export async function run(document) {
     const jmax = parseInt(params.get("jmax") || 100);
     const colormap = params.get("colormap") || "turbo";
     const dx = parseFloat(params.get("dx") || 5000); // m
+    const bottom_depth = parseFloat(params.get("bottom_depth") || 100); // m
+    let grav = parseFloat(params.get("grav") || 9.81);
+    let beta = parseFloat(params.get("beta") || 0);
+    let f = parseFloat(params.get("f") || 0);
+    let DeltaT = parseFloat(params.get("DeltaT") || 100);
+    let hmax = parseFloat(params.get("hmax") || 0.25);
+    let hmin = parseFloat(params.get("hmin") || -0.25);
 
     document.getElementById("colormap").value = colormap;
+    document.getElementById("grav").value = grav;
+    document.getElementById("f").value = f;
+    document.getElementById("beta").value = beta;
+    document.getElementById("DeltaT").value = DeltaT;
+    document.getElementById("hmax").value = hmax;
+    document.getElementById("hmin").value = hmin;
+    document.getElementById("xmax").innerHTML = (imax * dx)/1000; // km
+    document.getElementById("ymax").innerHTML = (jmax * dx)/1000; // km
+    document.getElementById("zmax").innerHTML = bottom_depth; // m
 
     const sz = [imax,jmax];
     var ntime = 0;
 
     let [mask_p, mask] = MallocArray(Int32Array,memory,base,sz);
+    let [fCoriolis_p, fCoriolis] = MallocArray(Float32Array,memory,base,sz);
+
     let [pressure_p, pressure] = MallocArray(Float32Array,memory,base,sz);
 
     let [u_p, u] = MallocArray(Float32Array,memory,base,[sz[0]+1,sz[1]]);
@@ -43,23 +61,24 @@ export async function run(document) {
     function step(timestamp) {
         let grav = parseFloat(document.getElementById("grav").value);
         let f = parseFloat(document.getElementById("f").value);
+        let beta = parseFloat(document.getElementById("beta").value);
         let DeltaT = parseFloat(document.getElementById("DeltaT").value);
-        let pmin = parseFloat(document.getElementById("pmin").value);
-        let pmax = parseFloat(document.getElementById("pmax").value);
+        let hmin = parseFloat(document.getElementById("hmin").value);
+        let hmax = parseFloat(document.getElementById("hmax").value);
         let show_velocity = document.getElementById("show_velocity").checked;
         let colormap = document.getElementById("colormap").value;
 
-        if (!isNaN(grav) && !isNaN(f) && !isNaN(pmin) && !isNaN(pmax) && !isNaN(DeltaT)) {
+        if (!isNaN(grav) && !isNaN(f) && !isNaN(hmin) && !isNaN(hmax) && !isNaN(DeltaT)) {
             //console.log("p ",pressure[140 + sz[0] * 40]);
-
             const result = julia_fluid_sim_step(
-                grav,f,dx,DeltaT,ntime,
-                mask_p,h_p,hu_p,hv_p,pressure_p,u_p,v_p,newu_p,newv_p);
+                grav,bottom_depth,f,beta,dx,DeltaT,ntime,
+                mask_p,fCoriolis_p,h_p,hu_p,hv_p,pressure_p,u_p,v_p,newu_p,newv_p);
+            //console.log("beta ",beta,fCoriolis[0]);
 
             ntime += 1;
 
             ctx.clearRect(0, 0, canvas.width, canvas.height);
-            pcolor(ctx,sz,res,pressure,mask,{pmin: pmin, pmax: pmax, cmap: colormap});
+            pcolor(ctx,sz,res,pressure,mask,{pmin: hmin, pmax: hmax, cmap: colormap});
 
             if (show_velocity) {
                 quiver(ctx,sz,res,u,v,mask,{subsample: 5, scale: 500});
