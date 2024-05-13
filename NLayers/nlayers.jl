ffefe23bbdab5e71dc72f3714c7f6b2ac0ceddc6
Alexander Barth <barth.alexander@gmail.com>
2024-05-13 12:39:55 +0200

    update

--- NLayers/nlayers.jl
+++ NLayers/nlayers.jl
@@ -87,12 +87,7 @@ end
 
 
 function nlayer_step(n,dx,dt,g,rho,P,h,hm,hu,u,z,bottom)
-    __nlayer_step(n,dx,dt,g,rho,P,h,hm,(hu,),(u,),z,bottom)
-end
-
-function __nlayer_step(n,dx,dt,g,rho,P,h,hm,huv,uv,z,bottom)
-    m = size(h)[end]
-    sz = size(k)[1:end-1]
+    imax,m = size(h)
 
     @inbounds for k = 1:m
         for i = 2:imax
@@ -134,5 +129,92 @@ function __nlayer_step(n,dx,dt,g,rho,P,h,hm,huv,uv,z,bottom)
         for i = 2:imax
             u[i,k] = u[i,k] - dt * (P[i,k]-P[i-1,k])/(rho[k]*dx)
         end
+    end
+end
+
+
+function __nlayer_step(n,dxy::NTuple{N},dt,g,fCoriolis,rho,P,h,hm,huv,uv,z,bottom) where N
+    m = size(h)[end]
+    sz = size(h)[1:end-1]
+
+    unitvecs = ntuple(i -> CartesianIndex(ntuple(==(i), Val(N))), Val(N))
+    I = CartesianIndices(sz)
+    Ifirst, Ilast = first(I), last(I)
+    I1 = oneunit(Ifirst)
+
+    @inbounds for k = 1:m
+        for (uvec,u,hu) = zip(unitvecs,uv,huv)
+            for ij = Ifirst+uvec:Ilast
+                #hu[ij,k] = (h[ij-uvec,k]+h[ij,k]) * u[ij,k]/2;
+                hu[ij,k] = (hm[ij-uvec,k]+hm[ij,k]) * u[ij,k]/2;
+            end
+        end
+
+        for (uvec,u,hu,dx) = zip(unitvecs,uv,huv,dxy)
+            for ij = Ifirst:Ilast
+                h[ij,k] = h[ij,k] - dt * (hu[ij+uvec,k] - hu[ij,k])/dx;
+            end
+        end
+    end
+
+    # z position of each layer interface
+    @inbounds for k = m:-1:1
+        for ij = Ifirst:Ilast
+            z[ij,k] = z[ij,k+1] + h[ij,k]
+        end
+    end
+
+    # Montgomery potential P
+    #
+    # P = p + ρ g z
+    #
+    # where p is the pressure, ρ the density, g the acceleration due to
+    # gravity and z the vertical coordinate.
+    #
+    # ∂P
+    # -- = g z
+    # ∂ρ
+    #
+
+    @inbounds for ij = Ifirst:Ilast
+        P[ij,1] = g*z[ij,1]*rho[1]
+        for k = 1:m-1
+            P[ij,k+1] = P[ij,k] + (rho[k+1]-rho[k]) * g * z[ij,k+1]
+        end
+    end
+
+    @inbounds for k = 1:m
+        for (uvec,u,dx) = zip(unitvecs,uv,dxy)
+            for ij = Ifirst+uvec:Ilast
+                u[ij,k] = u[ij,k] - dt * (P[ij,k]-P[ij-uvec,k])/(rho[k]*dx)
+            end
+        end
+    end
+
+    if length(uv) == 2
+        # Coriolis force
+
+        # # update velocities based on pressure gradient
+        # @inbounds for s = 0:1
+        #     if mod(n+s,2) == 0
+        #         for j = 1:size(mask,2)
+        #             for i = 2:size(mask,1)
+        #             ff = (fCoriolis[i,j] + fCoriolis[i-1,j])/2
+        #             vp = (v[i,j]+v[i,j+1]+v[i-1,j]+v[i-1,j+1])/4
+        #             u[i,j] = u[i,j] + (ff*vp - g * (η[i,j] - η[i-1,j])/Δx)*Δt
+        #         end
+        #     end
+        # else
+        #     for j = 2:size(mask,2)
+        #         for i = 1:size(mask,1)
+        #             ff = (fCoriolis[i,j] + fCoriolis[i,j-1])/2
+        #             up = (u[i,j]+u[i+1,j]+u[i,j-1]+u[i+1,j-1])/4
+        #             v[i,j] = v[i,j] + (-ff*up - g * (η[i,j] - η[i,j-1])/Δy)*Δt
+        #         end
+        #     end
+        # end
+    end
+end
+        
     end
 end
